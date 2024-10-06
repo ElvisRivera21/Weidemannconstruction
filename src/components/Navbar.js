cf0a6a407fbb9ed08bef32cf3c8e391c1ae2db75
Elvis Rivera <therealelvisrivera@gmail.com>
2024-10-06 18:08:29 -0500

    basic page complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-// src/components/Navbar.js
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -16,36 +15,6 @@ function Navbar() {
             Weidmann Construction
           </span>
         </Link>
-
-        {/* Mobile Menu Button */}
-        <button
-          className="text-gray-900 dark:text-white md:hidden"
-          onClick={() => setNavbarOpen(!navbarOpen)}
-        >
-          {/* Hamburger Icon */}
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            {navbarOpen ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
-            ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            )}
-          </svg>
-        </button>
 
         {/* Navigation Links */}
         <div
@@ -65,25 +34,6 @@ function Navbar() {
                 {path === '/' ? 'Home' : path.replace('/', '').charAt(0).toUpperCase() + path.slice(2)}
               </Link>
             ))}
-
-            {/* Get Started Button */}
-            <Link
-              to="/get-started"
-              className="bg-blue-700 text-white px-4 py-2 rounded-lg hover:bg-blue-800 mt-2 md:mt-0 md:ml-4"
-            >
-              Get Started
-            </Link>
-
-            <Link to="/services" className="text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500 py-2 md:py-0">
-  Services
-</Link>
-
-            <Link to="/portfolio" className="text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500 py-2 md:py-0">
-  Portfolio
-</Link>
-
-            
-
           </div>
         </div>
       </div>
